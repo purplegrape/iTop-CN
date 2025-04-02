10c3f6d2ff35e84aad19c9231699f567d0950a76
purplegrape <purplegrape4@gmail.com>
2025-04-02 14:23:30 +0800

    update dockerfile

--- docker/2.7/entrypoint.sh
+++ docker/2.7/entrypoint.sh
@@ -4,7 +4,7 @@ set -e -x
 
 DST=/data/iTop
 if [ ! -d $DST ];then
-  unzip -q -n -d $DST /iTop-2.7.8-10409.zip
+  unzip -q -n -d $DST /opt/iTop-2.7.zip
   mkdir -p $DST/web/{conf,data,env-production,env-production-build,log}
   chown -R www-data.www-data $DST/web/{conf,data,env-production,env-production-build,log}
   ln -sf $DST/web /var/www/html

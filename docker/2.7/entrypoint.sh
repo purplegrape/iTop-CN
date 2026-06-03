11c3b7e8d7f57b161a6b9132936791596b4e4d7b
purple grape <purplegrape4@gmail.com>
2026-06-03 17:51:56 +0800

    Update entrypoint.sh

--- docker/2.7/entrypoint.sh
+++ docker/2.7/entrypoint.sh
@@ -4,6 +4,8 @@ if [ ! -d /data/itop ];then
   rsync -aqu /usr/src/itop/ /data/itop/
   mkdir -p /data/itop/{conf,data,log,env-production,env-production-build,env-test,env-test-build}
   chown -R www-data:www-data /data/itop/{conf,data,log,env-production,env-production-build,env-test,env-test-build}
+  find /data/itop -type d -exec chmod 755 {} \;
+  find /data/itop -type f -exec chmod 644 {} \;
 fi
 
 if [ ! -L /var/www/html ];then
